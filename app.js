4848e958cacb46871cd068ca958f699b937f3e67
sourav-yadav-11 <pratapsouravyadav2255@gmail.com>
2023-03-31 20:44:52 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req,res){
+    const query = (req.body.city);
+    const apiKey = "57ec86e41b69d9f23936a256f2722985";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ unit;
+
+    https.get(url, function(response){
+
+        response.on("data", function (data) { 
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDiscription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imgURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+
+            res.write("<p>The weather is currently "+ weatherDiscription +".</p>");
+            res.write("<h1>The tempteture in "+ query +" is "+ temp +" degree Celcius.</h1>");
+            res.write("<img src="+imgURL+">");
+            res.send();
+         });
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server running on Port:3000.");
+});
+
+
+
+
+
+// API Key: 57ec86e41b69d9f23936a256f2722985
---
+++ readme.txt
@@ -0,0 +1,6 @@
+OPEN WEATHER MAP:-
+-> Create Account
+-> Varify email
+-> Go to the current weather Data (API doc) and copy the url of city name and save it somewhere for letter use.
+-> Copy the API key and save it for latter use.
+-> Now you have all the required element t built the web app. Thus you can proceed with the coding part.
